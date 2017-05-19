9b6c715b86df6149888be781abfcbc348f7e2ac5
matveu <i.matviichuk@umh.com.ua>
2017-05-19 17:23:30 +0300

    + save images with relative path

--- parser.php
+++ parser.php
@@ -7,26 +7,34 @@ require '/vendor/autoload.php';
 class DomParser
 {
     private $page;
-    public $url;
+    private $host;
+    private $scheme;
+    private $url;
+
+    const IMAGE_TYPES = [
+        IMAGETYPE_GIF  => '.gif',
+        IMAGETYPE_JPEG => '.jpg',
+        IMAGETYPE_PNG  => '.png'
+    ];
 
     public function __construct($url)
     {
+        if (empty($url) || !is_string($url)) {
+            throw new \ErrorException('Please, set valid URL!');
+        }
+
         $this->url = $url;
     }
 
     private function getPageData()
     {
-        if (empty($this->url) || !is_string($this->url)) {
-            throw new \ErrorException('Please, set valid URL!');
-        }
-
         $page = file_get_html($this->url);
 
         if (!$page) {
             throw new \ErrorException("Can't get data from page. Please, check the entering URL and try again.");
         }
 
-        return $page;
+        $this->page = $page;
     }
 
     private function curlGetContents($imgSrc)
@@ -42,42 +50,54 @@ class DomParser
         return $data;
     }
 
-    private function modifierPrependHttps($string, $subStr)
+    private function parseUrl()
     {
-        if (!empty($string) && !empty($subStr)) {
-            $length = strlen($subStr);
-            if (substr($string, 0, $length) === $subStr) {
-                return 'https:' . $string;
-            }
+        $this->scheme   = parse_url($this->url, PHP_URL_SCHEME);
+        $this->host     = parse_url($this->url, PHP_URL_HOST);
+    }
 
-            return $string;
-        } else {
-            return '';
+    private function makeCorrectSrc($src)
+    {
+        if (substr($src, 0, 2) === '//') {
+            return $this->scheme . ':' . $src;
+        } elseif (substr($src, 0, 1) === '/') {
+            return $this->scheme . '://' . $this->host . $src;
+        }
+
+        return $src;
+    }
+
+    private function createDir()
+    {
+        if (!file_exists(__DIR__ . '\parsed_img')) {
+            if (!mkdir(__DIR__ . '\parsed_img', 0777, true)) {
+                throw new \ErrorException("Can't create new directory in: " . __DIR__);
+            };
         }
     }
 
     public function run()
     {
         echo "Starting..." . PHP_EOL;
-        $this->page = $this->getPageData();
 
-        if (!file_exists(__DIR__ . '\parsed_img')) {
-            if(!mkdir(__DIR__ . '\parsed_img', 0777, true)){ throw new \ErrorException("Can't create new directory in: " . __DIR__); };
-        }
+        $this->getPageData();
+        $this->createDir();
+        $this->parseUrl();
 
         $countSavedImg = 0;
-        $imageTypes = [
-            IMAGETYPE_GIF  => '.gif',
-            IMAGETYPE_JPEG => '.jpg',
-            IMAGETYPE_PNG  => '.png'
-        ];
 
         foreach ($this->page->find('img') as $img) {
-            $img->src = $this->modifierPrependHttps($img->src, '//');
-            if (!$this->curlGetContents($img->src)){ echo "Image url is wrong: $img->src" . PHP_EOL; continue;}
-            if (!array_key_exists((exif_imagetype($img->src)) ?: 0, $imageTypes)) { echo "Incorrect image type: $img->src" . PHP_EOL; continue;}
-            $imagePath = "\\parsed_img\\" . bin2hex(openssl_random_pseudo_bytes(10)) . $imageTypes[exif_imagetype($img->src)];
+            if (empty($img->src)){ echo "Image src is empty." . PHP_EOL; continue;}
+
+            $img->src = $this->makeCorrectSrc($img->src);
+
+            if (!$this->curlGetContents($img->src)){ echo "Image src is wrong: $img->src" . PHP_EOL; continue;}
+            if (!array_key_exists((exif_imagetype($img->src)) ?: 0, self::IMAGE_TYPES)) { echo "Incorrect image type: $img->src" . PHP_EOL; continue;}
+
+            $imagePath = "\\parsed_img\\" . bin2hex(openssl_random_pseudo_bytes(10)) . self::IMAGE_TYPES[exif_imagetype($img->src)];
+
             if (!file_put_contents(__DIR__ . $imagePath, $this->curlGetContents($img->src), FILE_APPEND | LOCK_EX)) { echo "Can't write image to file: $img->src" . PHP_EOL; continue;}
+
             $countSavedImg += 1;
         }
 
