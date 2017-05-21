756d88a39f2898d53401b0a76471f87ce7d52449
matveu <matveu1990@gmail.com>
2017-05-21 07:57:15 +0300

    Added the ability to parse images from the whole site

--- parser.php
+++ parser.php
@@ -8,6 +8,8 @@ class DomParser
     private $host;
     private $scheme;
     private $url;
+    private $visitedPages = [];
+    private $pathToFolder;
 
     const IMAGE_TYPES = [
         IMAGETYPE_GIF  => '.gif',
@@ -22,6 +24,13 @@ class DomParser
         }
 
         $this->url = $url;
+        $this->init();
+    }
+
+    private function init()
+    {
+        $this->parseUrl();
+        $this->createFolder();
     }
 
     private function getPageData()
@@ -29,7 +38,7 @@ class DomParser
         $page = file_get_html($this->url);
 
         if (!$page) {
-            throw new \ErrorException("Can't get data from page. Please, check the entering URL and try again.");
+            throw new \ErrorException("Can't get data from page: $this->url");
         }
 
         $this->page = $page;
@@ -48,13 +57,26 @@ class DomParser
         return $data;
     }
 
+    private function curlUrlExists($url)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_NOBODY, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $status = [];
+        preg_match('/HTTP\/.* ([0-9]+) .*/', curl_exec($ch), $status);
+
+        return ($status[1] == 200);
+    }
+
     private function parseUrl()
     {
         $this->scheme   = parse_url($this->url, PHP_URL_SCHEME);
         $this->host     = parse_url($this->url, PHP_URL_HOST);
     }
 
-    private function makeCorrectSrc($src)
+    private function getAbsoluteSrc($src)
     {
         if (substr($src, 0, 2) === '//') {
             return $this->scheme . ':' . $src;
@@ -65,41 +87,89 @@ class DomParser
         return $src;
     }
 
-    private function createDir()
+    private function createFolder()
     {
         if (!file_exists(__DIR__ . '\parsed_img')) {
             if (!mkdir(__DIR__ . '\parsed_img', 0777, true)) {
-                throw new \ErrorException("Can't create new directory in: " . __DIR__);
+                throw new \ErrorException("Can't create new folder in: " . __DIR__);
             };
         }
+        $this->pathToFolder = __DIR__ . '\parsed_img';
+    }
+
+    private function parsePage()
+    {
+        echo "Starting... " . $this->url, PHP_EOL;
+
+        $this->visitedPages[] = $this->url;
+        $this->getPageData();
+        $this->saveImages();
+        $this->goToAnotherPage();
+    }
+
+    private function saveImages()
+    {
+        $qtySavedImg = 0;
+
+        foreach ($this->page->find('img') as $img) {
+            if (empty($img->src)) {
+                echo "Image src is empty.", PHP_EOL;
+                continue;
+            }
+
+            $img->src = $this->getAbsoluteSrc($img->src);
+
+            if (!$this->curlGetContents($img->src)) {
+                echo "Image src is wrong: $img->src", PHP_EOL;
+                continue;
+            }
+            if (!array_key_exists((exif_imagetype($img->src)) ?: 0, self::IMAGE_TYPES)) {
+                echo "Incorrect image type: $img->src", PHP_EOL;
+                continue;
+            }
+
+            $imageName = bin2hex(openssl_random_pseudo_bytes(10)) . self::IMAGE_TYPES[exif_imagetype($img->src)];
+
+            if (!file_put_contents($this->pathToFolder . '\\' . $imageName, $this->curlGetContents($img->src), FILE_APPEND | LOCK_EX)) {
+                echo "Can't save image to folder: $img->src", PHP_EOL;
+                continue;
+            }
+
+            $qtySavedImg += 1;
+        }
+
+        echo "Number of saved images: $qtySavedImg", PHP_EOL;
+    }
+
+    private function goToAnotherPage()
+    {
+        foreach ($this->page->find('a') as $link) {
+            if (!$this->checkLink($link->href)) {
+                continue;
+            }
+            $this->parsePage();
+        }
+    }
+
+    private function checkLink($link)
+    {
+        if (substr($link, 0, 2) === '//') {
+            $fullLink = $this->scheme . ":" . $link;
+        } elseif (substr($link, 0, 1) === '/') {
+            $fullLink = $this->scheme . "://" . $this->host . $link;
+        } elseif (strpos($link, $this->host) !== false) {
+            $fullLink = $link;
+        } else {
+            return false;
+        }
+
+        return (!in_array($fullLink, $this->visitedPages)) ? (($this->curlUrlExists($fullLink)) ? $this->url = $fullLink : false) : false;
     }
 
     public function run()
     {
-        echo "Starting..." , PHP_EOL;
+        $this->parsePage();
 
-        $this->getPageData();
-        $this->createDir();
-        $this->parseUrl();
-
-        $countSavedImg = 0;
-
-        foreach ($this->page->find('img') as $img) {
-            if (empty($img->src)){ echo "Image src is empty." , PHP_EOL; continue;}
-
-            $img->src = $this->makeCorrectSrc($img->src);
-
-            if (!$this->curlGetContents($img->src)){ echo "Image src is wrong: $img->src" , PHP_EOL; continue;}
-            if (!array_key_exists((exif_imagetype($img->src)) ?: 0, self::IMAGE_TYPES)) { echo "Incorrect image type: $img->src" , PHP_EOL; continue;}
-
-            $imagePath = "\\parsed_img\\" . bin2hex(openssl_random_pseudo_bytes(10)) . self::IMAGE_TYPES[exif_imagetype($img->src)];
-
-            if (!file_put_contents(__DIR__ . $imagePath, $this->curlGetContents($img->src), FILE_APPEND | LOCK_EX)) { echo "Can't write image to file: $img->src" , PHP_EOL; continue;}
-
-            $countSavedImg += 1;
-        }
-
-        echo "Total saved images: $countSavedImg" , PHP_EOL;
-        echo "Directory: " . __DIR__ . "\\parsed_img" , PHP_EOL;
+        echo "Path to saved pictures: $this->pathToFolder", PHP_EOL;
     }
 }
